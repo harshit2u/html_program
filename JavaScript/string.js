253069d8fdd851b579915b2c78ebe7f0a405f540
harshit upadhyay <uharshit580@gmail.com>
2025-01-20 10:49:00 +0530

    adding format.html file

---
+++ JavaScript/string.js
@@ -0,0 +1,7 @@
+var str = "Hello Harshit is here";
+console.log(str);
+console.log(str.length);
+console.log(str.charAt(1));
+console.log(str.startsWith("Hello"));
+console.log(str.endsWith("is"));
+console.log(str.endsWith("here"));
