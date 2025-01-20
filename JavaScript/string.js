54f0fa50c934ec45996cb037e76d9946840b7400
harshit upadhyay <uharshit580@gmail.com>
2025-01-20 10:55:42 +0530

    20 jan

--- JavaScript/string.js
+++ JavaScript/string.js
@@ -1,7 +1,9 @@
-var str = "Hello Harshit is here";
+var str = "Hello Harshit is here ";
 console.log(str);
 console.log(str.length);
 console.log(str.charAt(1));
 console.log(str.startsWith("Hello"));
 console.log(str.endsWith("is"));
 console.log(str.endsWith("here"));
+console.log(str.includes("harshit"));
+console.log(str.repeat(5));
