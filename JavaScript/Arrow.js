921a3eb0d4f010f1fb7ec5741f5dd3f136fd2e05
harshit upadhyay <uharshit580@gmail.com>
2025-01-20 11:50:52 +0530

    20 jan

---
+++ JavaScript/Arrow.js
@@ -0,0 +1,8 @@
+const displayInfo=()=>{
+    console.log("This is a Arrow Function");
+}
+const getInfo=()=>{
+    console.log("This is a get Info function");
+}
+displayInfo();
+getInfo();
---
+++ JavaScript/for-each.js
@@ -0,0 +1,4 @@
+let studentList = ["Harshit", "Amit", "Rahul", "Rohit"];
+for(let s of studentList){
+    console.log(s);
+}
